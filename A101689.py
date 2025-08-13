60bfe9dab4135ff19b6c4b98d6540cb7afadd510
0x7b <bbbbbbbIniad80x7b@users.noreply.github.com>
2025-08-13 23:28:45 +0900

    first

---
+++ A101689.py
@@ -0,0 +1,19 @@
+import math
+
+def fib(n):
+    lst = [0,1]
+    if n == 0:
+        return 0
+    for i in range(1,n + 1):
+        lst[i % 2] = lst[i % 2] + lst[(i + 1) % 2]
+    return lst[i % 2]
+
+result = []
+sum = 0
+n = input("n = ")
+
+for i in range(1, int(n) + 1):
+    result.append(fib(i))
+    sum += 1/(math.prod(result))
+
+print(sum)
